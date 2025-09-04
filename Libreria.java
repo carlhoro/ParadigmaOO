8b594ee0004667f5e67f36e5f027792ec369664b
carlhoro <chrodriguez.bmdc@gmail.com>
2025-09-04 14:25:43 -0500

    Se crea proyecto springboot

---
+++ Libreria.java
@@ -0,0 +1,14 @@
+public class Libreria {
+ 
+    public String name;
+
+    public void cambiarNombre(String newName){
+        name = newName;
+    }
+
+}
+
+Libreria libreria1 = new Libreria();
+
+libreria1.name = "asdf";
+
