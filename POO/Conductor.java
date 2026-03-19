c697732763e022b776cfa379423cd7db3a3eedc7
INGJEISSON <innovasistemas@gmail.com>
2026-03-19 17:35:07 -0500

    Clase creada y testeada

---
+++ POO/Conductor.java
@@ -0,0 +1,23 @@
+public class Conductor extends Empleado{
+
+    private String placa;
+    private String licencia;
+
+    @Override
+    public Boolean iniciarJornada() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iniciarJornada'");
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    
+    
+}
